0ee62b8d796e758ddf75ca7d9606ada8914886fc
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-24 10:37:02 +0900

    環境変数関係のものをアップデート

--- process.d.ts
+++ process.d.ts
@@ -2,15 +2,13 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXTAUTH_URL: string;
     NEXTAUTH_SECRET: string;
-    GITHUB_ID: string;
-    GITHUB_SECRET: string;
-    FACEBOOK_ID: string;
-    FACEBOOK_SECRET: string;
-    TWITTER_ID: string;
-    TWITTER_SECRET: string;
     GOOGLE_ID: string;
     GOOGLE_SECRET: string;
-    AUTH0_ID: string;
-    AUTH0_SECRET: string;
+    SHEET_ID: string;
+    GOOGLE_SERVICE_ACCOUNT_EMAIL: string;
+    GOOGLE_PRIVATE_KEY: string;
+    NEXT_PUBLIC_VERIFICATION_DOMAIN: string;
+    NEXT_PUBLIC_DISCORD_TOKEN: string;
+    DISCORD_CHANNED_ID: string;
   }
 }
