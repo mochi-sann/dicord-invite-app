844b40a2cd2c99cacd678dba7154f7bbddfa249a
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-24 19:39:36 +0900

    update url

--- src/projectData.ts
+++ src/projectData.ts
@@ -1,5 +1,5 @@
 export const projectData = {
-  url: "https://join.mochi33.com",
+  url: "https://join-vl.mochi33.com",
   siteName: "Join Virtual life",
   discord: {
     channelId: process.env.DISCORD_CHANNED_ID || "",
