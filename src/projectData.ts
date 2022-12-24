dc8c69e11962e500955fb483781aeb705c713f76
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-24 21:04:16 +0900

    urlをアップデート

--- src/projectData.ts
+++ src/projectData.ts
@@ -1,5 +1,5 @@
 export const projectData = {
-  url: "https://join-vl.mochi33.com",
+  url: "https://joinvl.mochi33.com",
   siteName: "Join Virtual life",
   discord: {
     channelId: process.env.DISCORD_CHANNED_ID || "",
